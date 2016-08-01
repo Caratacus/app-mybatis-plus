b11acdea6f64d0df55808812a8300bfea26c7892
Caratacus <330952486@qq.com>
2016-08-01 16:31:42 +0800

    更新mybatis plus

--- src/main/java/com/app/mybatisplus/MybatisDefaultParameterHandler.java
+++ src/main/java/com/app/mybatisplus/MybatisDefaultParameterHandler.java
@@ -1,17 +1,31 @@
 /**
+
  * Copyright (c) 2011-2014, hubin (jobob@qq.com).
+
  *
+
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+
  * use this file except in compliance with the License. You may obtain a copy of
+
  * the License at
+
  *
+
  * http://www.apache.org/licenses/LICENSE-2.0
+
  *
+
  * Unless required by applicable law or agreed to in writing, software
+
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+
  * License for the specific language governing permissions and limitations under
+
  * the License.
+
  */
 package com.app.mybatisplus;
 
@@ -34,11 +48,17 @@ import com.app.mybatisplus.toolkit.TableInfo;
 import com.app.mybatisplus.toolkit.TableInfoHelper;
 
 /**
+ * 
  * <p>
+ * 
  * 自定义 ParameterHandler 重装构造函数，填充插入方法主键 ID
+ * 
  * </p>
  * 
+ *
+ * 
  * @author hubin
+ * 
  * @Date 2016-03-11
  */
 public class MybatisDefaultParameterHandler extends DefaultParameterHandler {
@@ -48,39 +68,70 @@ public class MybatisDefaultParameterHandler extends DefaultParameterHandler {
 	}
 
 	/**
+	 * 
 	 * <p>
+	 * 
 	 * 批量（填充主键 ID）
+	 * 
 	 * </p>
 	 * 
+	 *
+	 * 
 	 * @param ms
+	 * 
 	 * @param parameterObject
+	 * 
 	 *            插入数据库对象
+	 * 
 	 * @return
 	 */
 	protected static Object processBatch(MappedStatement ms, Object parameterObject) {
-		Collection<Object> parameters = getParameters(parameterObject);
-		if (parameters != null) {
-			List<Object> objList = new ArrayList<Object>();
-			for (Object parameter : parameters) {
-				objList.add(populateKeys(ms, parameter));
+		if (ms.getSqlCommandType() == SqlCommandType.INSERT) {
+			/**
+			 * 
+			 * 只处理插入操作
+			 */
+			Collection<Object> parameters = getParameters(parameterObject);
+			if (parameters != null) {
+				List<Object> objList = new ArrayList<Object>();
+				for (Object parameter : parameters) {
+					if (parameter instanceof Map) {
+						/* map 插入不处理 */
+						objList.add(parameter);
+					} else {
+						objList.add(populateKeys(ms, parameter));
+					}
+				}
+				return objList;
+			} else {
+				return populateKeys(ms, parameterObject);
 			}
-			return objList;
-		} else {
-			return populateKeys(ms, parameterObject);
 		}
+		return parameterObject;
 	}
 
 	/**
+	 * 
 	 * <p>
+	 * 
 	 * 处理正常批量插入逻辑
-	 * </p>
-	 * <p>
-	 * org.apache.ibatis.session.defaults.DefaultSqlSession$StrictMap 该类方法
-	 * wrapCollection 实现 StrictMap 封装逻辑
+	 * 
 	 * </p>
 	 * 
+	 * <p>
+	 * 
+	 * org.apache.ibatis.session.defaults.DefaultSqlSession$StrictMap 该类方法
+	 * 
+	 * wrapCollection 实现 StrictMap 封装逻辑
+	 * 
+	 * </p>
+	 * 
+	 *
+	 * 
 	 * @param parameter
+	 * 
 	 *            插入数据库对象
+	 * 
 	 * @return
 	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
@@ -102,38 +153,47 @@ public class MybatisDefaultParameterHandler extends DefaultParameterHandler {
 	}
 
 	/**
+	 * 
 	 * <p>
+	 * 
 	 * 填充主键 ID
+	 * 
 	 * </p>
 	 * 
+	 *
+	 * 
 	 * @param ms
+	 * 
 	 * @param parameterObject
+	 * 
 	 *            插入数据库对象
+	 * 
 	 * @return
 	 */
-	protected static Object populateKeys( MappedStatement ms, Object parameterObject ) {
-		if ( ms.getSqlCommandType() == SqlCommandType.INSERT ) {
-			TableInfo tableInfo = TableInfoHelper.getTableInfo(parameterObject.getClass());
-			if ( tableInfo != null && tableInfo.getIdType().getKey() >= 2 ){
-				MetaObject metaParam = ms.getConfiguration().newMetaObject(parameterObject);
-				Object idValue = metaParam.getValue(tableInfo.getKeyProperty());
-				/* 自定义 ID */
-				if ( idValue == null ) {
-					if ( tableInfo.getIdType() == IdType.ID_WORKER ) {
-						metaParam.setValue(tableInfo.getKeyProperty(), IdWorker.getId());
-					} else if ( tableInfo.getIdType() == IdType.UUID ) {
-						metaParam.setValue(tableInfo.getKeyProperty(), get32UUID());
-					}
+	protected static Object populateKeys(MappedStatement ms, Object parameterObject) {
+		TableInfo tableInfo = TableInfoHelper.getTableInfo(parameterObject.getClass());
+		if (null != tableInfo && null != tableInfo.getIdType() && tableInfo.getIdType().getKey() >= 2) {
+			MetaObject metaParam = ms.getConfiguration().newMetaObject(parameterObject);
+			Object idValue = metaParam.getValue(tableInfo.getKeyProperty());
+			/* 自定义 ID */
+			if (idValue == null) {
+				if (tableInfo.getIdType() == IdType.ID_WORKER) {
+					metaParam.setValue(tableInfo.getKeyProperty(), IdWorker.getId());
+				} else if (tableInfo.getIdType() == IdType.UUID) {
+					metaParam.setValue(tableInfo.getKeyProperty(), get32UUID());
 				}
-				return metaParam.getOriginalObject();
 			}
+			return metaParam.getOriginalObject();
 		}
 		return parameterObject;
 	}
 
 	/**
+	 * 
 	 * <p>
+	 * 
 	 * 获取去掉"-" UUID
+	 * 
 	 * </p>
 	 */
 	protected static synchronized String get32UUID() {
