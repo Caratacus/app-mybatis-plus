bf299eaa1aaa31cccbccf1c0500faa532a6926ce
Caratacus <330952486@qq.com>
2016-09-21 16:20:01 +0800

    序列化

--- src/main/java/com/app/framework/entity/IdWorkPrimaryKey.java
+++ src/main/java/com/app/framework/entity/IdWorkPrimaryKey.java
@@ -2,10 +2,13 @@ package com.app.framework.entity;
 
 import com.app.mybatisplus.annotations.Id;
 import com.app.mybatisplus.annotations.IdType;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 public class IdWorkPrimaryKey {
 
 	@Id(type = IdType.ID_WORKER)
+	@JsonSerialize(using = ToStringSerializer.class)
 	protected Long id;
 
 	public Long getId() {
